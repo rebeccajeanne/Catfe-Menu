25818d59612314d0d8d6c55790f9456fffd7f5dd
rebeccajeanne <lockburner.rebecca@gmail.com>
2025-10-01 18:52:24 -0400

    Update 10/1
    Added Athena & Artemis
    Changed Tilly adoption status
    Updated arrival date wording on main bios

--- js/bios.js
+++ js/bios.js
@@ -3,7 +3,7 @@ const getID = document.getElementById.bind(document);
         var w = $(window).width();
         let bg = getID("bgPage");
 
-        /*
+        
         //bio A open and close
         document.getElementById("nameA").addEventListener("click", openA);
         let bioA = getID("bioA_container");
@@ -38,7 +38,7 @@ const getID = document.getElementById.bind(document);
             }
           }
         }
-        */
+        
 
         //bio B open and close
         document.getElementById("nameB").addEventListener("click", openB);
@@ -324,7 +324,6 @@ const getID = document.getElementById.bind(document);
         
 
         //bio J open and close
-        /*
         document.getElementById("nameJ").addEventListener("click", openJ);
         let bioJ = getID("bioJ_container");
         let mbioJ = getID("mbioJ_container");
@@ -356,7 +355,7 @@ const getID = document.getElementById.bind(document);
               bg.classList.remove("modal_open");
             }
           }
-        }*/
+        }
 
         //bio K open and close
         document.getElementById("nameK").addEventListener("click", openK);
